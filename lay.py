bcf578809e9e67e4f39d3484454eb979a274fc11
sangr123 <begin772@gmail.com>
2023-05-13 16:36:30 +0900

    1st

--- lay.py
+++ lay.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_excel("C:/temp/streamlit/lda.xlsx", index_col=0)
+# df = pd.read_excel("C:/temp/streamlit/gpt.xlsx", index_col=0)
+df = pd.read_excel("C:/temp/streamlit/gpt_p.xlsx", index_col=0)
 
 # 타이틀 출력
 st.title("🏢 기업 토픽 🏢")
@@ -15,40 +16,59 @@ search_query = st.sidebar.text_input("검색")
 # 검색어를 메인에 출력
 if search_query:
     st.header(search_query)
+# str1 = "2020"
+# str2 = "2021"
+# str3 = "2022"
 
+matching_columns20 = [col for col in df.columns if search_query + "2020" in col]
+matching_columns21 = [col for col in df.columns if search_query + "2021" in col]
+matching_columns22 = [col for col in df.columns if search_query + "2022" in col]
+
+# search_query_2020 = search_query + str1
+# search_query_2021 = search_query + str
+# search_query_2022 = search_query + "2022"
 
 # 레이아웃 만들기
 # 3개의 열을 생성
-col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns((3,3,3))
 
 # 첫 번째 열에 내용 추가
 with col1:
     st.header("📋2020")
-    if search_query in df.index:
-       for text in df.loc[search_query,'2020']:
-           st.write(text)
+    if not search_query:
+        st.write("검색어를 입력해주세요.")
     else:
-       st.write('검색 결과가 없습니다')
+        if not matching_columns20:
+            st.write("검색 결과가 없습니다.")
+        else:
+            st.write(df[matching_columns20])
+       
 
 # 두 번째 열에 내용 추가
 with col2:
     st.header("📋2021")
-    if search_query in df.index:
-       for text in df.loc[search_query,'2021']:
-           st.write(text)
+    if not search_query:
+        st.write("검색어를 입력해주세요.")
     else:
-       st.write('검색 결과가 없습니다')
+        if not matching_columns21:
+            st.write("검색 결과가 없습니다.")
+        else:
+            st.write(df[matching_columns21])
 
 # 세 번째 열에 내용 추가
 with col3:
     st.header("📋2022")
-    if search_query in df.index:
-       for text in df.loc[search_query,'2022']:
-           st.write(text)
+    if not search_query:
+        st.write("검색어를 입력해주세요.")
     else:
-       st.write('검색 결과가 없습니다')
+        if not matching_columns22:
+            st.write("검색 결과가 없습니다.")
+        else:
+            st.write(df[matching_columns22])
+
 
 # streamlit run lay.py
+
 
 
 
